3959c438deb9e80882ab4d2469c781e703c5afcf
Maros Zatko <mzatko@redhat.com>
2013-08-22 16:18:50 +0200

    basic cmdline parser tests for P2V clients NewMain

---
+++ p2v/client/test/test_newmain.rb
@@ -0,0 +1,51 @@
+require 'minitest/autorun'
+require 'virt-p2v/ui/main'
+
+WD = File.expand_path File.dirname(__FILE__)
+CMDLINE_TEST = File.join(WD, "cmdline_test")
+CMDLINE_DEFAULT = File.join(WD, "cmdline_default")
+
+class TestNewMainDry < MiniTest::Unit::TestCase
+  def setup
+    @nm = VirtP2V::UI::NewMain.new dry=true
+  end
+
+  def test_cmdline_parse_d
+    params = @nm.parse_cmdline(CMDLINE_DEFAULT)
+    assert_equal params, {}
+  end
+
+  def test_cmdline_parse_t
+    params = @nm.parse_cmdline(CMDLINE_TEST)
+    assert_equal params, {"test" => "foo"}
+  end
+
+  def test_cmdline_parse_noval
+    params = @nm.parse_cmdline
+    assert_equal params, {}
+  end
+end
+
+
+class TestNewMain < MiniTest::Unit::TestCase
+  def setup
+    @nm = VirtP2V::UI::NewMain.new
+  end
+
+  def test_cmdline_parse_d
+    params = @nm.parse_cmdline(CMDLINE_DEFAULT)
+    assert_equal params, {}
+  end
+
+  def test_cmdline_parse_t
+    params = @nm.parse_cmdline(CMDLINE_TEST)
+    assert_equal params, {"test" => "foo"}
+  end
+
+  def test_cmdline_parse_noval
+    params = @nm.parse_cmdline
+    assert_equal params, {}
+  end
+end
+
+
