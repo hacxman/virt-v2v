5463b50913cd488387b5e6e250d6d845d6390250
Maros Zatko <mzatko@redhat.com>
2013-08-26 10:15:51 +0200

    p2v client: shrink TestNewMain class

--- p2v/client/test/test_newmain.rb
+++ p2v/client/test/test_newmain.rb
@@ -27,24 +27,9 @@ class TestNewMainDry < MiniTest::Unit::TestCase
 end
 
 
-class TestNewMain < MiniTest::Unit::TestCase
+class TestNewMain < TestNewMainDry
   def setup
     @nm = VirtP2V::UI::NewMain.new
-  end
-
-  def test_cmdline_parse_d
-    params = @nm.parse_cmdline(CMDLINE_DEFAULT)
-    assert_equal params, {}
-  end
-
-  def test_cmdline_parse_t
-    params = @nm.parse_cmdline(CMDLINE_TEST)
-    assert_equal params, {"test" => "foo"}
-  end
-
-  def test_cmdline_parse_noval
-    params = @nm.parse_cmdline
-    assert_equal params, {}
   end
 end
 
