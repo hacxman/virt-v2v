bab344a8626b9060b3aaa1ed233b8a81bd2912f5
Maros Zatko <mzatko@redhat.com>
2014-01-17 18:39:04 +0100

    p2v: refactor get_object from NewMain to gtk.rb

--- p2v/client/lib/virt-p2v/ui/gtk.rb
+++ p2v/client/lib/virt-p2v/ui/gtk.rb
@@ -50,5 +50,144 @@ if Kernel.const_defined?(:NOGUI) && NOGUI == true
     Cursor::Type = VirtP2V::UI::NeverMind
     Cursor::Type::X_CURSOR = VirtP2V::UI::NeverMind
   end
+
+  module VirtP2V
+  module UI
+    class SomeListW < NeverMind
+      def append(*args)
+        @items ||= []
+        @items << []
+        self
+      end
+
+      def _items
+        @items ||= []
+        @items
+      end
+
+      def _select(idx)
+        @idx = idx
+      end
+
+      def _selection
+        @idx ||= 0
+        @idx
+      end
+
+      def []=(idx, item)
+        @items.last[idx] = item
+        self
+      end
+
+      def clear
+        @items.clear
+      end
+
+      def each(&block)
+        @items.each do |item|
+          # block accepts three params:
+          # model, path, iter
+          # since we know nothing about model and path
+          # we'll fill only item as iter
+          block.call(nil, nil, item)
+        end
+      end
+    end
+
+    class ConvertProfileW < NeverMind
+      def active_iter
+        cpl = @main.get_object("convert_profile_list")
+        cpl._items[cpl._selection]
+      end
+    end
+
+    class NetworkDeviceListViewW < NeverMind
+      def selection
+        self
+      end
+
+      def selected
+        # TODO: this needs a proper selection
+        # now it's only for a testing purpouse
+        # but later we need to reflect reality
+        # and not blindly return first device
+        @main.get_object("network_device_list")._items.first
+      end
+    end
+
+    class ConnectErrorW < NeverMind
+      def text
+        @text || ''
+      end
+
+      def text=(str)
+        @text = str
+        unless str == ''
+          puts "Error connecting: '#{str}'"
+          puts "Giving up."
+          exit(3)
+        end
+      end
+    end
+
+    class ConvertStatusW < NeverMind
+      def text
+        @text || ''
+      end
+
+      def text=(str)
+        @text = str
+        puts "conversion status changed to: '#{str}'"
+        STDOUT.flush
+        if str =~ /failure|error/i
+          puts "Giving up."
+          exit(4)
+        end
+      end
+    end
+
+    class SomeTextFieldW < NeverMind
+      def text
+        @text || ""
+      end
+
+      def text=(str)
+        @text = str
+      end
+
+      def secondary_icon_tooltip_text=(str)
+        puts "Error in '#{@name}': '#{str}'"
+      end
+    end
+
+    def self.widget_class_factory(widget_name)
+      widgets = Hash[ *(["network_device_list",
+            "convert_profile_list", "convert_network_list",
+            "convert_fixed_list", "convert_removable_list"].flat_map do |_l|
+              [_l, SomeListW]
+            end) ]
+      .merge(
+      Hash[ *(["server_hostname", "server_username", "server_password",
+              "convert_name", "convert_cpus", "convert_memory",
+              "ip_manual", "ip_address", "ip_prefix", "ip_gateway",
+              "ip_dns", "server_hostname", "server_username",
+              "server_password"].flat_map do |_l|
+                [_l, SomeTextFieldW]
+              end) ]
+            ).merge({
+          "convert_profile" => ConvertProfileW,
+          "network_device_list_view" => NetworkDeviceListViewW,
+          "convert_status" => ConvertStatusW,
+          "connect_error" => ConnectErrorW
+      })
+
+      if widgets.has_key? widget_name
+        widgets[widget_name]
+      else
+        NeverMind
+      end
+    end
+  end
+  end
 end
 
